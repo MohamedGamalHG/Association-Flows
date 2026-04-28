7d5dc6ee7198b166848d090f547ff2c022b0fed8
FAWRY\Mohamed.Gammal <mohamed.gammal@fawry.com>
2026-04-28 22:26:57 +0300

    feat:add logic about customer

--- src/main/java/com/mg/Association_Flows/user/controller/UserController.java
+++ src/main/java/com/mg/Association_Flows/user/controller/UserController.java
@@ -3,10 +3,11 @@ package com.mg.Association_Flows.user.controller;
 import com.mg.Association_Flows.user.domain.entity.User;
 import com.mg.Association_Flows.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
@@ -15,8 +16,26 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+    @GetMapping("/getUserById/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable UUID id){
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
     @PostMapping
     public User createUser(@RequestBody User user){
         return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable UUID id,@RequestBody User user){
+        return ResponseEntity.ok(userService.updateUser(id,user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteUser(@PathVariable UUID id){
+        return ResponseEntity.ok(userService.deleteUser(id));
     }
 }
--- src/main/java/com/mg/Association_Flows/user/service/UserService.java
+++ src/main/java/com/mg/Association_Flows/user/service/UserService.java
@@ -3,7 +3,11 @@ package com.mg.Association_Flows.user.service;
 import com.mg.Association_Flows.user.domain.entity.User;
 import com.mg.Association_Flows.user.domain.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +15,38 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public User createUser(User user){
+    public User createUser(User user) {
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(UUID id) {
+        return findUser(id);
+    }
+
+    public User updateUser(UUID id, User user) {
+        User findedUser = findUser(id);
+        // this to update user that get from DB with data that will change to it
+        // that come from request
+        BeanUtils.copyProperties(user, findedUser, "id");
+        return userRepository.save(findedUser);
+    }
+
+    public boolean deleteUser(UUID id) {
+        findUser(id);
+        userRepository.deleteById(id);
+        return true;
+    }
+
+    private User findUser(UUID id) {
+        return userRepository.findById(id).orElseThrow(this::message);
+    }
+
+    private RuntimeException message() {
+        return new RuntimeException("User Not Found");
     }
 
 }
