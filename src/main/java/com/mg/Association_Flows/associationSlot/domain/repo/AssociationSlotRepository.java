eb306f8fcd4e6a3d8c37acb97ad635ff7a2fc174
MohamedGamalHG <mohamedgemmyhamzagaber@gmail.com>
2026-05-03 00:08:57 +0300

    feat:add the best query and remove the other because in past will get all association not specific association

--- src/main/java/com/mg/Association_Flows/associationSlot/domain/repo/AssociationSlotRepository.java
+++ src/main/java/com/mg/Association_Flows/associationSlot/domain/repo/AssociationSlotRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -15,14 +16,14 @@ public interface AssociationSlotRepository extends BaseRepository<AssociationSlo
 
     List<AssociationSlot> findByAssociationId(UUID associationId);
 
-    @Query("select slot.isVacant from AssociationSlot slot where  slot.association.id = :associationId and slot.isVacant = true")
+    @Query("select slot.isVacant from AssociationSlot slot where  slot.association.id = :associationId and slot.isReserved = true")
     Boolean checkIfAnyUserAssignToOrder(@Param("associationId") UUID associationId);
 
     Boolean existsByTurnOrder(int orderId);
 
-    @Query("select slot from AssociationSlot slot where " +
-            " slot.user.id is null and slot.isVacant is false and slot.turnOrder is null")
-    List<AssociationSlot> getAllAssociationSlotsThatNotAssignToUser();
+    @Query("SELECT s FROM AssociationSlot s WHERE s.association.id = :assocId AND s.turnOrder = :order AND s.user IS NULL")
+    Optional<AssociationSlot> findSpecificSlotToAssign(@Param("assocId") UUID assocId, @Param("order") int order);
+
 
 //    @Query("select slot from AssociationSlot slot where  slot.association.id = ?1 and slot.isVacant = true")
 //    AssociationSlot checkIfAnyUserAssignToOrder( UUID associationId);
