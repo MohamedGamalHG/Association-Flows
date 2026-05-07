196077f60cb988f8767b986b091b489888fb0239
MohamedGamalHG <mohamedgemmyhamzagaber@gmail.com>
2026-05-08 00:38:37 +0300

    feat:add new api to payment controller

--- src/main/java/com/mg/Association_Flows/payment/controller/PaymentController.java
+++ src/main/java/com/mg/Association_Flows/payment/controller/PaymentController.java
@@ -4,10 +4,10 @@ import com.mg.Association_Flows.payment.domain.dto.PaymentDto;
 import com.mg.Association_Flows.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/payment")
@@ -20,5 +20,18 @@ public class PaymentController {
     public ResponseEntity<PaymentDto> requestToPay(@RequestBody PaymentDto paymentDto) {
         return ResponseEntity.ok().body(paymentService.requestToPay(paymentDto));
     }
+
+    @PostMapping("/{paymentId}")
+    public ResponseEntity<Boolean> confirmPayment(@PathVariable UUID paymentId, @RequestBody PaymentDto paymentDto) {
+        return ResponseEntity.ok().body(paymentService.confirmPayment(paymentId,paymentDto));
+    }
+
+    @GetMapping("/all-payemnt-to-confirme/{assocId}")
+    public ResponseEntity<List<PaymentDto>> getAllPaymentConfirmed(@PathVariable UUID assocId) {
+        return ResponseEntity.ok().body(paymentService.getListOfPaymentToConfirmedByAssociationId(assocId));
+    }
+
+
+
 
 }
