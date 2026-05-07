31cb9b746df6a6d4d1e52e96330790e2f1da142b
MohamedGamalHG <mohamedgemmyhamzagaber@gmail.com>
2026-05-08 00:39:32 +0300

    fix:update logic of requestToPay and add some new method

--- src/main/java/com/mg/Association_Flows/payment/domain/repo/PaymentRepository.java
+++ src/main/java/com/mg/Association_Flows/payment/domain/repo/PaymentRepository.java
@@ -4,8 +4,10 @@ import com.mg.Association_Flows.payment.domain.dto.PaymentDto;
 import com.mg.Association_Flows.payment.domain.entity.Payment;
 import com.mg.Association_Flows.util.BaseRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -16,6 +18,10 @@ public interface PaymentRepository extends BaseRepository<Payment> {
 
     Optional<Payment> findByAssociationSlotId(UUID association_slot_Id);
 
-    @Query("select p from Payment p where p.associationSlot.association_id = :association_id")
-    Optional<List<Payment>> getAllPaymentByAssociationId(UUID association_id);
+    @Query("select p from Payment p where p.associationSlot.association.id = :assocId")
+    Optional<List<Payment>> getAllPaymentByAssociationId(@Param("assocId")UUID association_id);
+
+    // AND p.isUsedForPayout = false
+    @Query("SELECT SUM(p.amount) FROM Payment p WHERE p.associationSlot.association.id = :assocId AND p.status = 'CONFIRMED'")
+    BigDecimal calculateAvailablePool(@Param("assocId") UUID assocId);
 }
--- src/main/java/com/mg/Association_Flows/payment/service/PaymentService.java
+++ src/main/java/com/mg/Association_Flows/payment/service/PaymentService.java
@@ -1,6 +1,8 @@
 package com.mg.Association_Flows.payment.service;
 
+import com.mg.Association_Flows.association.enums.AssociationStatus;
 import com.mg.Association_Flows.association.service.AssociationService;
+import com.mg.Association_Flows.associationSlot.domain.dtos.AssociationSlotDto;
 import com.mg.Association_Flows.associationSlot.domain.entity.AssociationSlot;
 import com.mg.Association_Flows.associationSlot.service.AssociationSlotService;
 import com.mg.Association_Flows.payment.domain.dto.PaymentDto;
@@ -12,9 +14,10 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -25,24 +28,73 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final PaymentMapper paymentMapper;
-    private final AssociationSlotService  associationSlotService;
+    private final AssociationSlotService associationSlotService;
     private final AssociationService associationService;
 
     public PaymentDto requestToPay(PaymentDto paymentDto) {
+        UUID associationId = null;
+        if (paymentDto.getAssociationSlot().getId() != null) {
+            associationId = associationSlotService.getAssociationSlotById(paymentDto.getAssociationSlot().getId()).getAssociation().getId();
+        }else
+            throw new IllegalArgumentException("Association slot not found");
+
+        BigDecimal numberOfMonthPay = associationService.monthlyAmountOfAssociation(associationId);
+
+        // if the amount that come in request after divid it  to numberOfMonthPay it has any fraction
+        // then it pay number less than the numberOfMonthPay or if pay 3 month for example
+        // then it will pay it well because if i divid it then the number will be good no fraction
+
+        // i need to check if amount that come not less than , so it should pay the
+        // monthly amount or multiplies of monthly amount
+        boolean dividesEvenly = paymentDto.getAmount().remainder(numberOfMonthPay).compareTo(BigDecimal.ZERO) == 0;
+        if (!dividesEvenly)
+            throw new RuntimeException("you should pay the monthly amount or multiplies");
+
+        boolean isAssociationActive = associationService.statusOfAssociation(associationId) == AssociationStatus.ACTIVE;
+
+        if(!isAssociationActive)
+            throw new RuntimeException("this association is not active");
+
+
+        LocalDate dateOfPay = paymentDto.getPaymentDate().toLocalDateTime().toLocalDate();
+        LocalDate startDataOfAssociation = associationService.startDateOfAssociation(associationId);
+        LocalDate endDataOfAssociation = associationService.endDateOfAssociation(associationId);
+
+        if(!dateOfPay.isAfter(startDataOfAssociation) || !dateOfPay.isBefore(endDataOfAssociation))
+            throw  new RuntimeException("date of payment request should be between start date & end date of association");
+
+
         Payment payment = paymentMapper.mapToEntity(paymentDto);
         paymentRepository.save(payment);
         return paymentDto;
     }
 
-    public PaymentDto findPaymentByAssociationSlotId(UUID associationSlotId) {
-        Optional<Payment> payment = paymentRepository.findByAssociationSlotId(associationSlotId);
-        if (payment.isPresent()) {
-            return paymentMapper.mapToDTO(payment.get());
+    @Transactional
+    public boolean distributeMonthlyPayout(UUID associationId, int turnOrder,LocalDate currentDataOfMonth) {
+
+        try {
+            AssociationSlotDto targetSlot = associationSlotService
+                    .findByAssociationIdAndTurnOrder(associationId, turnOrder);
+
+            if (targetSlot.getIsPayoutDone()) {
+                throw new RuntimeException("this order is already payout");
+            }
+
+            BigDecimal totalAmountOfAssociation = associationService.totalPoolAmountOfAssociation(associationId);
+            BigDecimal totalAmountPayedUntilNow = paymentRepository.calculateAvailablePool(associationId);
+
+            if (totalAmountOfAssociation.compareTo(totalAmountPayedUntilNow) < 0)
+                throw new RuntimeException("the total number of payed until now not covered to payout");
+
+            associationSlotService.markSlotAsPaidOut(targetSlot.getId());
+
+            return true;
+        }catch (Exception e){
+            throw new RuntimeException(e);
         }
-        throw new RuntimeException("There is no payment with association slot id " + associationSlotId);
     }
 
-    public List<PaymentDto> getAllPaymentToConfirmedByAssociationId(UUID associationId) {
+    public List<PaymentDto> getListOfPaymentToConfirmedByAssociationId(UUID associationId) {
         Optional<List<Payment>> payments = paymentRepository.getAllPaymentByAssociationId(associationId);
         if (payments.isPresent()) {
             return payments.get().stream()
@@ -53,7 +105,7 @@ public class PaymentService {
     }
 
     @Transactional
-    public boolean confirmPaymentFromManager(UUID paymentId,PaymentDto paymentDto) {
+    public boolean confirmPayment(UUID paymentId,PaymentDto paymentDto) {
         // should update confirmation_date , status of payment , optional to add note
         Optional<Payment> payment = paymentRepository.findById(paymentId);
         if (payment.isPresent()) {
@@ -74,6 +126,7 @@ public class PaymentService {
 
 
             paymentRepository.save(payment.get());
+            return true;
         }
         throw new RuntimeException("There is no payment with this id " + paymentId);
     }
